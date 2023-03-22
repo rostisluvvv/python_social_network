d5e43da3ceee0ad1e9075308896d7c824fd4853b
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-22 13:34:19 +0300

    add new fields

--- py_social_net/profiles/models.py
+++ py_social_net/profiles/models.py
@@ -11,10 +11,21 @@ class UserNet(AbstractUser):
     --------
     This class inherits all the methods from AbstractUser.
     """
+
+    GENDER = (
+        ('Man', 'Man'),
+        ('Woman', 'Woman')
+    )
+
     middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to='user/avatar/',
                                blank=True,
                                null=True)
+    bio = models.TextField(blank=True, null=True)
+    github = models.CharField(max_length=500, blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER, default='Man')
+
 
