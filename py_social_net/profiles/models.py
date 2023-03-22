c861313f1704bd5493f59c6a54dfc9471c231853
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-22 12:09:53 +0300

    add UserNet model based AbstractUser

--- py_social_net/profiles/models.py
+++ py_social_net/profiles/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserNet(AbstractUser):
+    """
+    A model representing a user, extending the AbstractUser
+    class provided by Django.
+
+    Methods:
+    --------
+    This class inherits all the methods from AbstractUser.
+    """
+    middle_name = models.CharField(max_length=50)
+    first_login = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=14)
+    avatar = models.ImageField(upload_to='user/avatar/',
+                               blank=True,
+                               null=True)
+
