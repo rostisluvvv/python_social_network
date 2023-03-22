522d2184ce1db3f567071865c744220b725149db
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-22 14:50:21 +0300

    add routers.py + include routers in config/urls

--- py_social_net/config/urls.py
+++ py_social_net/config/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('', include('src.profiles.urls'))
+    path('api/v1/', include('src.routers'))
 ]
