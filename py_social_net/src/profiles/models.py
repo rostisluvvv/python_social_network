e4f7649f032cea173f437278d8e04a64d5feb5a3
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-23 10:31:49 +0300

    edd new model Technology

--- py_social_net/src/profiles/models.py
+++ py_social_net/src/profiles/models.py
@@ -16,7 +16,6 @@ class UserNet(AbstractUser):
         ('Man', 'Man'),
         ('Woman', 'Woman')
     )
-
     middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
@@ -27,5 +26,11 @@ class UserNet(AbstractUser):
     github = models.CharField(max_length=500, blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
     gender = models.CharField(max_length=6, choices=GENDER, default='Man')
+    technology = models.ManyToManyField('Technology', related_name='users')
 
 
+class Technology(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
