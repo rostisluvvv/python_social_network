1af6f8af9090e523843702a399111e6449c52217
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-23 11:31:25 +0300

    register Post model and Comment model

--- py_social_net/src/wall/admin.py
+++ py_social_net/src/wall/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from ..wall.models import Comment, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'author_user',
+        'moderation',
+        'create_date',
+        'published',
+        'view_count',
+        'id'
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(MPTTModelAdmin, admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_date', 'published', 'id')
+    mptt_level_indent = 15
