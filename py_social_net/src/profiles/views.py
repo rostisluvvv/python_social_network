03616fc48ae8e42bd5e21a647bb93decafd423bb
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-23 10:33:05 +0300

    update dockstrings

--- py_social_net/src/profiles/views.py
+++ py_social_net/src/profiles/views.py
@@ -6,12 +6,33 @@ from .serializers import GetUserNetSerializer, GetUserNetPublicSerializer
 
 
 class UserNerPublicView(ModelViewSet):
+    """
+    A viewset that provides CRUD operations for the UserNet
+    model with public access.
+
+    Attributes:
+    queryset: A queryset of all UserNet instances.
+    serializer_class: The serializer class used to serialize UserNet
+    instances.
+    permission_classes: The permission classes used to authenticate and
+    authorize requests.
+    """
     queryset = UserNet.objects.all()
     serializer_class = GetUserNetPublicSerializer
     permission_classes = [permissions.AllowAny]
 
 
 class UserNetView(ModelViewSet):
+    """
+    A viewset that provides CRUD operations for the UserNet model
+    with authenticated access.
+
+    Attributes:
+    serializer_class: The serializer class used to serialize UserNet
+    instances.
+    permission_classes: The permission classes used to authenticate and
+    authorize requests.
+    """
     serializer_class = GetUserNetSerializer
     permission_classes = [permissions.IsAuthenticated]
 
