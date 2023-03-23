5f72968ddb1c3a073e3b99e0198827391e7903d2
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-23 09:41:05 +0300

    add public profile serializer

--- py_social_net/src/profiles/serializers.py
+++ py_social_net/src/profiles/serializers.py
@@ -4,17 +4,33 @@ from .models import UserNet
 
 
 class GetUserNetSerializer(serializers.ModelSerializer):
-    """
-    Serializes UserNet model instances for retrieval over a network.
-    Excludes sensitive fields such as password, last login time,
-    and admin privileges.
-    """
+    """Serializes UserNet model."""
+    class Meta:
+        model = UserNet
+        exclude = (
+            'password',
+            'last_login',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions'
+        )
+
+
+class GetUserNetPublicSerializer(serializers.ModelSerializer):
+    """Serializes UserNet model for public."""
 
     class Meta:
         model = UserNet
-        exclude = ('password',
-                   'last_login',
-                   'is_active',
-                   'is_staff',
-                   'is_superuser')
-
+        exclude = (
+            'password',
+            'last_login',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'email',
+            'groups',
+            'user_permissions',
+            'phone'
+        )
