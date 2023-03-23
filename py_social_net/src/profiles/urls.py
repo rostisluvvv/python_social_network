eada6365c3facfd359fb0f4c89ca07704e3d4837
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-23 09:43:32 +0300

    add routing for PUT and GET request on prifile and public profile

--- py_social_net/src/profiles/urls.py
+++ py_social_net/src/profiles/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.GetUserNetView.as_view()),
+    path('profile/<int:pk>/', views.UserNetView.as_view({'get': 'retrieve',
+                                                         'put': 'update'})),
+    path('<int:pk>/', views.UserNerPublicView.as_view({'get': 'retrieve'})),
 
 ]
