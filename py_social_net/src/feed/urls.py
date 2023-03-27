8e62a2961e42c713b758ce84e171374e6a85f325
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-27 08:59:35 +0300

    add new url

--- py_social_net/src/feed/urls.py
+++ py_social_net/src/feed/urls.py
@@ -3,10 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    # path('post/<int:pk>/', views.PostView.as_view(
-    #     {'put': 'update',
-    #      'get': 'retrieve',
-    #      'delete': 'destroy'}
-    # )),
+    path('<int:pk>/', views.FeedView.as_view({'get': 'retrieve'}
+    )),
     path('', views.FeedView.as_view({'get': 'list'}))
 ]
